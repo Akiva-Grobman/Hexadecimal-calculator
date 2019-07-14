cde86d45a3f7071cccac51b2ed811588f832c536
yechielb2000 <51932344+yechielb2000@users.noreply.github.com>
2019-07-14 23:17:16 +0300

    added calculation methods

--- src/calculator/Hex.java
+++ src/calculator/Hex.java
@@ -6,22 +6,47 @@ public class Hex {
 	
 	public Hex (String hex)
 	{
-		int s = hex.length(), z = 0, x;
+		int s = hex.length(), z = 1, x, a = 0;
 		
 		 for (int i = 0; i < hex.length(); i++) {
 
-			 hex.charAt(hex.length()-z);
-			 
-			x = (hex.length()-z);
-			
-		}
+			x = hex.charAt(hex.length()-z);
 		
-			 z--;
+			if(x >= 'A' && x <= 'F')
+				x = x - 'A' + 10;
+			if(x >= '0' && x >= '9' )
+			    x = x - '0';
+		
+		
+			this.hex = x * (int)Math.pow(16, a);
+			a++;
+			
+			z++;
+		}
+
+		
 		
 	}
 	
+	public Hex multiply(Hex hex2) {
+		return null;
+	}
 	
+	public Hex divide(Hex hex2) {
+		return null;
+	}
 	
+	public Hex add(Hex hex2) {
+		return null;
+	}
+	
+	public Hex substract(Hex hex2) {
+		return null;
+	}
+	
+	public String toString() {
+		return "";
+	}
 	
 	
 	
