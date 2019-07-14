30d572b55bd00410d3d59e497cb7d84cdad53d15
akivav <44336769+akivav@users.noreply.github.com>
2019-07-14 23:24:26 +0300

    started on the calculation

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -4,24 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-
 import calculator.CustomButton.BUTTON_TYPE;
-import javafx.scene.chart.BubbleChart;
 
 public class Calculator extends JPanel {
 
 	private static final long serialVersionUID = -1340732273401597053L;
 	private Screen screen;
 	private NumberPanel numberPanel;
-	private Hex hex;
-	private String hexString;
-	private BUTTON_TYPE actionType;
-	private int oldNumber;
+	private Hex hex1 = null, hex2 = null;
+	private String numberString = "";
+	private BUTTON_TYPE actionType = null;
 	
 	public Calculator(int width, int height) {
 		screen = new Screen(width - (width / 100 * 20), height - (height / 100 * 95), this);
@@ -36,34 +30,39 @@ public class Calculator extends JPanel {
 		this.add(numberPanel, BorderLayout.CENTER);
 	}
 
-	public void calc() {
+	public void setHexs() {
+		setHex(numberString);
+		if(actionType != null) {
+			
+		}
+	}
+	
+	private void calculate() {
+		switch(actionType) {
 		
+		}
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 	}
 
-	public String getHex() {
-		return this.hexString;
+	public String getNumberString() {
+		return numberString;
+	}
+
+	public void setNumberString(String numberString) {
+		this.numberString = numberString;
 	}
 
 	public void setHex(String hex) {
-		this.hexString = hex;
+		if(hex1 == null) {
+			hex1 = new Hex(hex);
+		} else {
+			hex2 = new Hex(hex);
+		}
 	}
 
-	public int getOldNumber() {
-		return oldNumber;
-	}
-
-	public void setOldNumber(int oldNumber) {
-		this.oldNumber = oldNumber;
-	}
-
-	public BUTTON_TYPE getAction() {
-		return this.actionType;
-	}
-	
 	public void setAction(BUTTON_TYPE buttonType) {
 		this.actionType = buttonType;
 	}
--- src/calculator/NumberButton.java
+++ src/calculator/NumberButton.java
@@ -12,26 +12,37 @@ public class NumberButton extends JButton{
 
 	private static final long serialVersionUID = -5825753833125096648L;
 	private int value;
+	private String hex;
 	
 	public NumberButton(int value, Color color, Calculator calculator) {
+		// sets the graphics
 		this.setBackground(color);
 		this.value = value;
 		setBorder(BorderFactory.createLineBorder(Color.red));
+		// sets the button value(0-9,A-F)
 		if(value > 9) {
 			this.setText((char)(value) + "");
 		} else {
 			this.setText(value + "");
 		}
+		// adds the button listener
 		this.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				calculator.setHex(calculator.getHex() + value);
-				System.out.println(calculator.getHex());
+				calculator.setNumberString(calculator.getNumberString() + value);
 			}
 		});
 	}
 
 	public int getValue() {
 		return value;
+	}
+
+	public String getHex() {
+		return this.hex;
+	}
+
+	public void setHex(String hex) {
+		this.hex = hex;
 	}
 
 	public void paintComponent(Graphics g) {
